253a75f13ad2243e7bc641e7c72b6bf269141f83
Abdirahman Jama <abdirahman@Abdirahmans-MacBook-Pro.local>
2021-05-13 10:33:30 +0100

    Added List and AddItem Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import { Heading } from "@chakra-ui/react"
-
+import { Stack, HStack, VStack } from "@chakra-ui/react"
+import Add from './components/Add'
+import List from './components/List'
 
 function App() {
   return (
-  <Heading>Todo Application</Heading>
+    <VStack>
+      <Heading>Todo Application</Heading>
+      <List/>
+      <Add/>
+    </VStack>
   );
 }
 
